ea59816b7c878e675463a1beb5fe2e46773077f9
Giovanni <giovanni.sokenou2105@gmail.com>
2024-04-05 01:27:04 +0100

    ok

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) { 
-      $('.navbar').addClass('navbar-scrolled');
-    } else {
-      $('.navbar').removeClass('navbar-scrolled');
-    }
-  });
-});
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 50) { 
+//       $('.navbar').addClass('navbar-scrolled');
+//     } else {
+//       $('.navbar').removeClass('navbar-scrolled');
+//     }
+//   });
+// });
 
 
 
